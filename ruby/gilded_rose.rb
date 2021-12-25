8a9ac970de6c3fd88af9d8782765c99fe7a2a624
dave doolin <david.doolin@gmail.com>
2021-12-25 08:58:16 -0800

    extract quality modification into method

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -22,31 +22,35 @@ class GildedRose
     !concert?(name)
   end
 
-  def update_quality()
-    @items.each do |item|
-      if not_brie?(item.name) && not_concert?(item.name)
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+  def adjust_quality(item)
+    if not_brie?(item.name) && not_concert?(item.name)
+      if item.quality > 0
+        if item.name != "Sulfuras, Hand of Ragnaros"
+          item.quality = item.quality - 1
         end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if concert?(item.name)
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+      end
+    else
+      if item.quality < 50
+        item.quality = item.quality + 1
+        if concert?(item.name)
+          if item.sell_in < 11
+            if item.quality < 50
+              item.quality = item.quality + 1
             end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+          end
+          if item.sell_in < 6
+            if item.quality < 50
+              item.quality = item.quality + 1
             end
           end
         end
       end
+    end
+  end
+
+  def update_quality()
+    @items.each do |item|
+      adjust_quality(item)
 
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
